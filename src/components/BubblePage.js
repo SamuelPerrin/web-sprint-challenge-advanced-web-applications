d7b088f13c7948d42b103935c675160fec06b9df
SamuelPerrin <swperrin51@gmail.com>
2021-02-26 11:11:04 -0600

    get GET working sorta

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-
+import axiosWithAuth from "../helpers/axiosWithAuth";
+import axios from 'axios';
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    // console.log("in BubblePage's useEffect with", axiosWithAuth)
+
+    axios
+      .get(`http://localhost:5000/api/colors`, {
+        headers:{
+          'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
+        }
+      })
+      .then(res=> {
+        console.log(res);
+        setColorList(res.data)
+      })
+      .catch(err => {
+        console.log("GET ERR in BubblePage", {err});
+      })
+  },[]);
+
 
   return (
     <>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [loginValues, setLoginValues] = useState({username:'', password:''});
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
 
-  useEffect(()=>{
+  // useEffect(()=>{
+  //   // make a post request to retrieve a token from the api
+  //   // when you have handled the token, navigate to the BubblePage route
+  // });
+
+  const handleChange = e => {
+    setLoginValues({...loginValues, [e.target.name]:e.target.value})
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
     axios
-      .delete(`http://localhost:5000/api/colors/1`, {
-        headers:{
-          'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
-        }
-      })
-      .then(res=>{
-        axios.get(`http://localhost:5000/api/colors`, {
-          headers:{
-            'authorization': ""
-          }
-        })
-        .then(res=> {
-          console.log(res);
-        });
+      .post('http://localhost:5000/api/login', loginValues)
+      .then(res => {
         console.log(res);
+        setErrorMessage('');
+        localStorage.setItem('token',JSON.stringify(res.data.payload));
+        setLoginValues({username:'', password:''});
+        history.push('/bubbles');
       })
-  });
+      .catch(err => {
+        console.log('ERROR', {err});
+        setErrorMessage(err.response.data.error)
+      })
+  }
 
   return (
     <>
-      <h1>
-        Welcome to the Bubble App!
-        <p>Build a login page here</p>
-      </h1>
+      <form onSubmit={handleSubmit}>
+        <label>username
+          <input
+            name='username'
+            type='text'
+            value={loginValues.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>password
+          <input
+            name='password'
+            type='password'
+            value={loginValues.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type='submit'>Login</button>
+      </form>
+      {errorMessage ? <p style={{color:'red'}}>Username or Password not valid.</p> : null}
     </>
   );
 };
@@ -39,7 +63,7 @@ export default Login;
 
 //Task List:
 //1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
+//2. Add whatever state necessary for form functioning.
 //3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
-//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
+//4. If either the username or password is not displayed display EXACTLY the following words: Username or Password not valid.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
